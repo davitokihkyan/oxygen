cd60c48f8da277274ad067d81dfe56aa9cb98fe4
Davit Okhikyan <david.okhikyan@gmail.com>
2023-11-08 00:06:54 +0400

    added Laravel Breeze

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Araksya',
+            'email' => 'arsuvan73@gmail.com',
+            'password' => Hash::make('hayk2019')
+        ]);
+    }
+}
